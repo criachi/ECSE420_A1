1d8eac7558edb185b9e85ea8594475ccffee07aa
Christina <christina.riachi@mail.mcgill.ca>
2021-01-24 18:41:25 -0500

    did parts 1.1, 1.2, 1.3 using naive O(n^3) matrix multiplication algo for sequential and parallel algos

--- src/ca/mcgill/ecse420/a1/MatrixMultiplication.java
+++ src/ca/mcgill/ecse420/a1/MatrixMultiplication.java
@@ -1,11 +1,15 @@
 package ca.mcgill.ecse420.a1;
 
+import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
 
 public class MatrixMultiplication {
 
-  private static final int NUMBER_THREADS = 1;
+  private static final int NUMBER_THREADS = 4;
   private static final int MATRIX_SIZE = 2000;
 
   public static void main(String[] args) {
@@ -13,8 +17,32 @@ public class MatrixMultiplication {
     // Generate two random matrices, same size
     double[][] a = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
     double[][] b = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
-    sequentialMultiplyMatrix(a, b);
-    parallelMultiplyMatrix(a, b);
+//    double[][] a = {{1, 2, 3}, {4, 5, 2}, {0, 5, 3}};
+//    double[][] b = {{2, 2, 2}, {3, 3, 0}, {1, 4, 5}};
+    long seqStartTime = System.currentTimeMillis();
+    double[][] seqNonRecProd = sequentialMultiplyMatrix(a, b);
+    long seqEndTime = System.currentTimeMillis();
+
+
+    //double[][] seqRecProd = sequentialRecursiveMultiplyMatrix(a,b);
+
+    long pStartTime = System.currentTimeMillis();
+    double[][] parallelNonRecProd = parallelMultiplyMatrix(a,b);
+    long pEndTime = System.currentTimeMillis();
+
+//    System.out.println("Sequential non recursive , naive O(n^3)");
+//    for(double[] x: seqNonRecProd)
+//      System.out.println(Arrays.toString(x));
+
+    System.out.println("\nSequential time is " +
+        (seqEndTime - seqStartTime) + " milliseconds");
+
+//    System.out.println("Parallel non recursive , naive O(n^3)");
+//    for(double[] x: parallelNonRecProd)
+//      System.out.println(Arrays.toString(x));
+
+    System.out.println("\nParallel time is " +
+        (pEndTime - pStartTime) + " milliseconds");
   }
 
   /**
@@ -42,6 +70,19 @@ public class MatrixMultiplication {
     return product;
   }
 
+//  public static double[][] sequentialRecursiveMultiplyMatrix(double[][] a, double[][] b) {
+//    int r1 = a.length;
+//    int r2 = b.length;
+//    int c1 = a[0].length;
+//    int c2 = b[0].length;
+//
+//    int dimension = r1;
+//
+//    int size = dimension / 2;
+//    double[][] prod = new double[dimension][dimension];
+//    double[][] a11b11
+//  }
+
   /**
    * Returns the result of a concurrent matrix multiplication The two matrices are randomly
    * generated
@@ -50,16 +91,64 @@ public class MatrixMultiplication {
    * @param b is the second matrix
    * @return the result of the multiplication
    */
+  // we can parallelize it however we want:
+  // we can parallelize the row mult w/ each of the columns of the scnd matrix
+  // or even each elem multiplication, we just need to explain in report how we parallelized it
   public static double[][] parallelMultiplyMatrix(double[][] a, double[][] b) {
-    // just filler code to avoid error
     int r1 = a.length;
-    int c1 = a[0].length;
     int c2 = b[0].length;
-
     double[][] product = new double[r1][c2];
+    RecursiveAction mainTask = new MatrixMultiplyTask(a, b, product, 0, r1);
+    ForkJoinPool pool = new ForkJoinPool(NUMBER_THREADS);
+    pool.invoke(mainTask);
     return product;
   }
 
+  private static class MatrixMultiplyTask extends RecursiveAction {
+    private static final int DIMENSION_THRESHOLD = 20;
+    private double[][] a;
+    private double[][] b;
+    private int currRow;
+    private double[][] prod;
+    private int dimension;
+
+    public MatrixMultiplyTask(double[][] a, double[][] b, double[][] prod, int currRow, int dimension) {
+      this.a = a;
+      this.b = b;
+      this.prod = prod;
+      this.currRow = currRow;
+      this.dimension = dimension;
+    }
+
+    @Override
+    protected void compute() {
+      int r1 = a.length;
+      int c1 = a[0].length;
+      int c2 = b[0].length;
+
+      if(dimension < DIMENSION_THRESHOLD) {
+        for (int i = currRow; i < r1; i++) {
+          for (int j = 0; j < c2; j++) {
+            for (int k = 0; k < c1; k++) {
+              prod[i][j] += a[i][k] * b[k][j];
+            }
+          }
+        }
+      } else {
+        for(int i = 0; i < c2; i++) {
+          prod[currRow][i] = 0;
+          for(int j = 0; j < c1; j++) {
+            prod[currRow][i] += a[currRow][j]*b[j][i];
+          }
+        }
+
+        currRow++;
+        dimension--;
+        new MatrixMultiplyTask(a, b, prod, currRow, dimension).invoke();
+      }
+
+    }
+  }
   /**
    * Populates a matrix of given size with randomly generated integers between 0-10.
    *
@@ -78,3 +167,4 @@ public class MatrixMultiplication {
   }
 
 }
+
