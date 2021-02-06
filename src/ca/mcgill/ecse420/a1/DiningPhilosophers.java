8dc288e9348ffef8426dd2c729f705a5ba148c04
Christina <christina.riachi@mail.mcgill.ca>
2021-02-06 14:58:06 -0500

    fixed code for Q1 and made the plots

--- src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
+++ src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
@@ -6,87 +6,86 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class DiningPhilosophers {
-	
-	public static LinkedList<Integer> queue;
-	
-	public static void main(String[] args) {
 
-		int numberOfPhilosophers = 5;
-        Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
-        ReentrantLock[] chopsticks = new ReentrantLock[numberOfPhilosophers];
-        queue = new LinkedList<Integer>();
-        
-        ExecutorService executor = Executors.newCachedThreadPool();
-        
-        
-        for (int i = 0; i < numberOfPhilosophers; i++) {
-        	chopsticks[i] = new ReentrantLock();
+  public static LinkedList<Integer> queue;
+
+  public static void main(String[] args) {
+
+    int numberOfPhilosophers = 5;
+    Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
+    ReentrantLock[] chopsticks = new ReentrantLock[numberOfPhilosophers];
+    queue = new LinkedList<Integer>();
+
+    ExecutorService executor = Executors.newCachedThreadPool();
+
+    for (int i = 0; i < numberOfPhilosophers; i++) {
+      chopsticks[i] = new ReentrantLock();
+    }
+
+    for (int i = 0; i < numberOfPhilosophers; i++) {
+      philosophers[i] = new Philosopher(i, chopsticks[i],
+          chopsticks[(i + 1) % numberOfPhilosophers]);
+      executor.execute(philosophers[i]);
+    }
+
+    executor.shutdown();
+  }
+
+  public static class Philosopher implements Runnable {
+
+    private int number;
+    private ReentrantLock left;
+    private ReentrantLock right;
+
+    public Philosopher(int number, ReentrantLock left, ReentrantLock right) {
+      this.number = number;
+      this.left = left;
+      this.right = right;
+    }
+
+    @Override
+    public void run() {
+      boolean isFirst;
+
+      while (true) {
+
+        isFirst = false;
+
+        synchronized (queue) {
+          if (!queue.isEmpty() && queue.getFirst() == this.number) {
+            isFirst = true;
+          }
         }
-        
-        for (int i = 0; i < numberOfPhilosophers; i++) {
-        	philosophers[i] = new Philosopher(i, chopsticks[i], chopsticks[(i + 1)%numberOfPhilosophers]);
-        	executor.execute(philosophers[i]);
+
+        if (isFirst) {
+          if (left.tryLock()) {
+            if (right.tryLock()) {
+              //eat
+              System.out.println("Philosopher " + this.number + " ate.");
+
+              synchronized (queue) {
+                queue.removeFirst();
+              }
+
+              left.unlock();
+              right.unlock();
+
+            } else {
+              left.unlock();
+            }
+          }
         }
-        
-        executor.shutdown();
-	}
-
-	public static class Philosopher implements Runnable {
-
-		private int number;
-		private ReentrantLock left;
-		private ReentrantLock right;
-
-		public Philosopher(int number, ReentrantLock left, ReentrantLock right) {
-			this.number = number;
-			this.left = left;
-			this.right = right;
-		}
-
-		@Override
-		public void run() {		
-			boolean isFirst;
-			
-			while(true) {				
-				
-				isFirst = false;
-				
-				synchronized (queue) {
-					if (!queue.isEmpty() && queue.getFirst() == this.number) {
-						isFirst = true;
-					}
-				}
-				
-				if (isFirst) {
-					if (left.tryLock()) {
-						if (right.tryLock()) {
-							//eat
-							System.out.println("Philosopher " + this.number + " ate.");
-
-							synchronized (queue) {
-								queue.removeFirst();
-							}
-							
-							left.unlock();
-							right.unlock();
-							
-						}
-						else {
-							left.unlock();
-						}
-					}
-				}
-				// Add to queue if not empty
-				else {
-					synchronized (queue) {
-						if (!queue.contains(this.number)) {
-						queue.add(this.number);
-						System.out.println(DiningPhilosophers.queue);
-						System.out.println("Philosopher " + this.number + " added to queue.");
-						}
-					}
-				}				
-			}
-		}
-	}
+        // Add to queue if not empty
+        else {
+          synchronized (queue) {
+            if (!queue.contains(this.number)) {
+              queue.add(this.number);
+              System.out.println(DiningPhilosophers.queue);
+              System.out.println("Philosopher " + this.number + " added to queue.");
+            }
+          }
+        }
+      }
+    }
+  }
 }
--- src/ca/mcgill/ecse420/a1/MatrixMultiplication.java
+++ src/ca/mcgill/ecse420/a1/MatrixMultiplication.java
@@ -11,22 +11,19 @@ import java.util.concurrent.Executors;
 
 public class MatrixMultiplication {
 
-  private static final int PREFERRED_NUM_THREADS = 4;
-  private static int numThreads = 6;
+  private static int PREFERRED_NUM_THREADS = 8;
+  private static final int NUM_THREADS = 8;
   private static int MATRIX_SIZE = 2000;
   private static ExecutorService executor;
 
   public static void main(String[] args) {
     try {
-      testExecTimeVSNumThreads();
+//      testExecTimeVSNumThreads();
       testSeqAndParallelExecTimeVSMatrixSize();
     } catch (IOException e) {
       System.out.println("errored");
     }
 
-    // Generate two random matrices, same size
-//    double[][] a = generateRandomMatrix(matrixSize, matrixSize);
-//    double[][] b = generateRandomMatrix(matrixSize, matrixSize);
 //    double[][] a = {{1, 2, 3}, {4, 5, 2}, {0, 5, 3}};
 //    double[][] b = {{2, 2, 2}, {3, 3, 0}, {1, 4, 5}};
 //    long seqStartTime = System.currentTimeMillis();
@@ -38,26 +35,54 @@ public class MatrixMultiplication {
 //    long pStartTime = System.currentTimeMillis();
 //    double[][] parallelNonRecProd = parallelMultiplyMatrix(a,b);
 //    long pEndTime = System.currentTimeMillis();
-
+//
 //    System.out.println("Sequential non recursive , naive O(n^3)");
 //    for(double[] x: seqNonRecProd)
 //      System.out.println(Arrays.toString(x));
-
+//
 //    System.out.println("\nSequential time is " +
 //        (seqEndTime - seqStartTime) + " milliseconds");
-
+//
 //    System.out.println("Parallel non recursive , naive O(n^3)");
 //    for(double[] x: parallelNonRecProd)
 //      System.out.println(Arrays.toString(x));
-
+//
 //    System.out.println("\nParallel time is " +
 //        (pEndTime - pStartTime) + " milliseconds");
   }
 
-  // testing out square matrix sizes 100, 200, 500, 1000, 2000
-  // with 4 threads
+  // varying numThreads from 2 to 8 for matrixSize = 2000
+  public static void testExecTimeVSNumThreads() throws IOException {
+    double[][] a = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
+    double[][] b = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
+
+    FileWriter csvWriter = new FileWriter("Part4.csv");
+    csvWriter.append("NumThreads");
+    csvWriter.append(",");
+    csvWriter.append("ExecutionTime");
+    csvWriter.append("\n");
+
+    for (int i = 2; i <= NUM_THREADS; i++) {
+      System.out.println("Part 1.4");
+      System.out.println("Running 2000 by 2000 matrix multiplication in parallel with " + i + " threads.");
+      executor = Executors.newFixedThreadPool(i);
+      long pStartTime = System.currentTimeMillis();
+      parallelMultiplyMatrix(a, b);
+      long pEndTime = System.currentTimeMillis();
+      long duration = pEndTime - pStartTime;
+      csvWriter.append(String.valueOf(i));
+      csvWriter.append(",");
+      csvWriter.append(String.valueOf(duration));
+      csvWriter.append("\n");
+    }
+
+    csvWriter.flush();
+    csvWriter.close();
+  }
+
+  // testing out square matrix sizes 100, 200, 500, 1000, 2000, 4000
   public static void testSeqAndParallelExecTimeVSMatrixSize() throws IOException {
-    int[] matrixSizes = {100, 200, 500, 1000, 2000};
+    int[] matrixSizes = {100, 200, 500, 1000, 2000, 4000};
 
     FileWriter csvWriter = new FileWriter("Part5.csv");
     csvWriter.append("Sequential");
@@ -67,17 +92,18 @@ public class MatrixMultiplication {
     csvWriter.append("ExecutionTime");
     csvWriter.append("\n");
 
-    double[][] a = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
-    double[][] b = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
-
     // Sequential multiplication
     for(int matrixSize : matrixSizes) {
+      double[][] a = generateRandomMatrix(matrixSize, matrixSize);
+      double[][] b = generateRandomMatrix(matrixSize, matrixSize);
       System.out.println("Part 1.5");
       System.out.println("Running " + matrixSize + " by " + matrixSize + " matrix multiplication sequentially");
       long seqStartTime = System.currentTimeMillis();
       sequentialMultiplyMatrix(a, b);
       long seqEndTime = System.currentTimeMillis();
       long duration = seqEndTime - seqStartTime;
+      System.out.println(seqStartTime);
+      System.out.println(seqEndTime);
       csvWriter.append(String.valueOf(matrixSize));
       csvWriter.append(",");
       csvWriter.append(String.valueOf(duration));
@@ -97,6 +123,8 @@ public class MatrixMultiplication {
 
     // Parallel multiplication
     for(int matrixSize : matrixSizes) {
+      double[][] a = generateRandomMatrix(matrixSize, matrixSize);
+      double[][] b = generateRandomMatrix(matrixSize, matrixSize);
       System.out.println("Part 1.5");
       System.out.println("Running " + matrixSize + " by " + matrixSize + " matrix multiplication in parallel with " + PREFERRED_NUM_THREADS + " threads.");
       executor = Executors.newFixedThreadPool(PREFERRED_NUM_THREADS);
@@ -105,35 +133,6 @@ public class MatrixMultiplication {
       long pEndTime = System.currentTimeMillis();
       long duration = pEndTime - pStartTime;
       csvWriter.append(String.valueOf(matrixSize));
-      csvWriter.append(",");
-      csvWriter.append(String.valueOf(duration));
-      csvWriter.append("\n");
-    }
-
-    csvWriter.flush();
-    csvWriter.close();
-  }
-
-  // varying numThreads from 2 to 6 for matrixSize = 2000
-  public static void testExecTimeVSNumThreads() throws IOException {
-    double[][] a = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
-    double[][] b = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
-
-    FileWriter csvWriter = new FileWriter("Part4.csv");
-    csvWriter.append("NumThreads");
-    csvWriter.append(",");
-    csvWriter.append("ExecutionTime");
-    csvWriter.append("\n");
-
-    for (int i = 2; i <= numThreads; i++) {
-      System.out.println("Part 1.4");
-      System.out.println("Running 2000 by 2000 matrix multiplication in parallel with " + i + " threads.");
-      executor = Executors.newFixedThreadPool(numThreads);
-      long pStartTime = System.currentTimeMillis();
-      parallelMultiplyMatrix(a, b);
-      long pEndTime = System.currentTimeMillis();
-      long duration = pEndTime - pStartTime;
-      csvWriter.append(String.valueOf(i));
       csvWriter.append(",");
       csvWriter.append(String.valueOf(duration));
       csvWriter.append("\n");
@@ -152,14 +151,12 @@ public class MatrixMultiplication {
    * @return the result of the multiplication
    */
   public static double[][] sequentialMultiplyMatrix(double[][] a, double[][] b) {
-    int r1 = a.length;
-    int c1 = a[0].length;
-    int c2 = b[0].length;
+    int dim = a.length;
 
-    double[][] product = new double[r1][c2];
-    for (int i = 0; i < r1; i++) {
-      for (int j = 0; j < c2; j++) {
-        for (int k = 0; k < c1; k++) {
+    double[][] product = new double[dim][dim];
+    for (int i = 0; i < dim; i++) {
+      for (int j = 0; j < dim; j++) {
+        for (int k = 0; k < dim; k++) {
           product[i][j] += a[i][k] * b[k][j];
         }
       }
@@ -177,19 +174,22 @@ public class MatrixMultiplication {
    * @return the result of the multiplication
    */
   public static double[][] parallelMultiplyMatrix(double[][] a, double[][] b) {
-    int r1 = a.length;
-    int c2 = b[0].length;
-    double[][] product = new double[r1][c2];
+    int dim = a.length;
+    double[][] product = new double[dim][dim];
 
     // row-view of matrix multiplication
     // each task will compute 1 cell value of the product matrix
-    for (int i = 0; i < r1; i++) { // for each row of first matrix
-      for (int j = 0; j < c2; j++) { // for each column of second matrix
+    for (int i = 0; i < dim; i++) { // for each row of first matrix
+      for (int j = 0; j < dim; j++) { // for each column of second matrix
         executor.execute(new MatrixMultiplyTask(a[i], b, i, j, product));
       }
     }
 
-    executor.shutdown();
+    try {
+      executor.shutdown();
+    } catch(Exception e) {
+      System.out.println("shutdown error");
+    }
 
     // Wait until all tasks are finished
     while (!executor.isTerminated()) {
